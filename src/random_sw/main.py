81cab3d283e9d10f644ccbad96f0652d727c00db
Josh Nygaard <josh.nygaard@gmail.com>
2025-03-07 12:26:50 -0500

    The start of several different ways to do this.

--- src/random_sw/main.py
+++ src/random_sw/main.py
@@ -1,6 +1,6 @@
 """Get random Star Wars themed data."""
 
-import random
+from random import choice, randint
 
 import yaml
 
@@ -24,20 +24,35 @@ _name_suffix_list_file = "src/random_sw/data/name_suffixes.yaml"
 with open(_name_suffix_list_file) as f:
     _name_suffixes = yaml.safe_load(f)
 
+_street_name_list_file = "src/random_sw/data/street_names.yaml"
+
+with open(_street_name_list_file) as f:
+    _street_names = yaml.safe_load(f)
+
+_street_type_list_file = "src/random_sw/data/street_types.yaml"
+
+with open(_street_type_list_file) as f:
+    _street_types = yaml.safe_load(f)
+
 
 def get_random_family_name():
     """Get a random Star Wars themed family name."""
-    return random.choice(_family_names)
+    return choice(_family_names)
 
 def get_random_given_name():
     """Get a random Star Wars themed given name."""
-    return random.choice(_given_names)
+    return choice(_given_names)
 
 def get_random_name_prefix():
     """Get a random Star Wars themed name prefix."""
-    return random.choice(_name_prefixes)
+    return choice(_name_prefixes)
 
 
 def get_random_name_suffix():
     """Get a random Star Wars themed name suffix."""
-    return random.choice(_name_suffixes)
+    return choice(_name_suffixes)
+
+
+def get_random_street_address():
+    """Get a random Star Wars themed street address."""
+    return f"{randint(1, 9999)} {choice(_street_names)} {choice(_street_types)}"
